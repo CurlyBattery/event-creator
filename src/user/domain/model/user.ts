587bcd5ba49ccfcabf815356df6cb7cf4c275c81
Kosyrev143 <artemkosyrev93@gmail.com>
2025-08-25 22:32:57 +0500

    ref: (user) model 3

--- src/user/domain/model/user.ts
+++ src/user/domain/model/user.ts
@@ -1,19 +1,11 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-export enum SubscriptionPlan {
-  FREE,
-  PRO,
-}
-
 export class UserWithoutPassword {
   @ApiProperty({ example: 'uuid' })
   id?: string;
 
   @ApiProperty({ example: 'defaultmail@gmail.com' })
   email: string;
-
-  @ApiProperty({ example: 'FREE', default: SubscriptionPlan.FREE })
-  subscriptionPlan: SubscriptionPlan[];
 
   @ApiPropertyOptional({ example: true })
   isVerified?: boolean;
