789f8543a2fbcfdca0807600e85a5fafc3444da8
Kosyrev143 <artemkosyrev93@gmail.com>
2025-09-01 23:31:50 +0500

    feat: (subcsription-plan) add controller

--- src/user/infra/controllers/subcription-plan.controller.ts
+++ src/user/infra/controllers/subcription-plan.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Patch } from '@nestjs/common';
+import { SubscriptionPlanService } from '@user/infra/ports/subscription-plan.service';
+import { AuthUser } from '@common/decorators/auth-user.decorator';
+import { AccessTokenPayload } from '@auth/infra/types/access-token.payload';
 
 @Controller()
 export class SubscriptionPLanController {
-  constructor() {}
+  constructor(
+    private readonly subscriptionPlanService: SubscriptionPlanService,
+  ) {}
+
+  @Patch('upgrade')
+  upgrade(@AuthUser() user: AccessTokenPayload) {
+    return this.subscriptionPlanService.upgrade(user.sub);
+  }
+
+  @Patch('me')
+  me(@AuthUser() user: AccessTokenPayload) {
+    return this.subscriptionPlanService.me(user.sub);
+  }
+
+  @Patch('cancel')
+  cancel(@AuthUser() user: AccessTokenPayload) {
+    return this.subscriptionPlanService.cancel(user.sub);
+  }
 }
